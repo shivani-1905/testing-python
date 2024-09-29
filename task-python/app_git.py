65031d5f58257d6fcf8406fa5be1269b655d5d8d
Your Name <your.email@example.com>
2024-09-29 21:49:00 +0530

    first

--- task-python/app_git.py
+++ task-python/app_git.py
@@ -42,4 +42,4 @@ def github_webhook():
         return jsonify(error_response), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
